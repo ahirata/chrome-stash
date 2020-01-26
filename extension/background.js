6d16d8d4c4a1f09ca4d3fd0e2d9b40201a3404d6
Arthur Hirata <arthur_hirata@yahoo.com.br>
2020-01-26 17:24:34 -0300

    Move functions to background.js to allow hotkeys

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,66 @@
 'use strict';
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, changedTab) {
   if (!changeInfo.url) {
     return;
   }
 
-  chrome.storage.local.get('openingTabs', function(data) {
-    if (!data.openingTabs) {
+  chrome.storage.local.get('tabs', function(data) {
+    if (!data.tabs) {
       return;
     }
 
-    if (data.openingTabs.includes(tabId)) {
+    if (data.tabs.includes(tabId)) {
       chrome.tabs.discard(tabId);
-      chrome.storage.local.set({'openingTabs': data.openingTabs.filter(tab => tab != tabId)})
+      chrome.storage.local.set({'tabs': data.tabs.filter(tab => tab != tabId)})
     }
   })
 });
+
+chrome.commands.onCommand.addListener(executeCommand);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  executeCommand(request.command)
+});
+
+function executeCommand(command) {
+  if (command === 'apply_stash') {
+    applyStash();
+  } else if (command === 'stash') {
+    stash();
+  }
+}
+
+function applyStash() {
+  chrome.storage.local.get('urls', data => {
+    if (!data.urls) {
+      return;
+    }
+
+    chrome.tabs.query({ currentWindow: true, pinned: false}, tabs => {
+      let urls = data.urls.filter(url => !tabs.some(tab => tab.url === url));
+      if (urls.length === 0) {
+        return;
+      }
+
+      let tabsCreated = [];
+      urls.forEach(url => {
+        chrome.tabs.create({ url: url, active: false}, tab => {
+          tabsCreated.push(tab.id);
+          chrome.storage.local.set({ 'tabs': tabsCreated });
+        });
+      });
+    });
+  });
+}
+
+function stash() {
+  chrome.tabs.query({ currentWindow: true, pinned: false}, tabs => {
+    if (tabs.length === 0) {
+      return;
+    }
+
+    chrome.tabs.remove(tabs.map(tab => tab.id));
+    chrome.storage.local.set({'urls': tabs.map(tab => tab.url)});
+  });
+}
--- extension/popup.js
+++ extension/popup.js
@@ -1,45 +1,11 @@
 'use strict';
 
-function stash(event) {
-  chrome.tabs.query({ currentWindow: true, pinned: false}, tabs => {
-    let pages = tabs.reduce((acc, tab) => {
-      acc['ids'].push(tab.id)
-      acc['urls'].push(tab.url)
-      return acc
-    }, {'ids': [], urls: [] });
+document.getElementById('stash').addEventListener('click', () => {
+  chrome.runtime.sendMessage({'command': "stash"});
+  window.close();
+});
 
-    chrome.tabs.remove(pages['ids']);
-    chrome.storage.local.set({'urls': pages['urls']}, () => {
-      window.close();
-    });
-  });
-}
-
-function apply() {
-  chrome.storage.local.get('urls', data => {
-    if (!data.urls) {
-      window.close();
-    }
-
-    chrome.tabs.query({ currentWindow: true, pinned: false}, tabs => {
-      let urls = data.urls.filter(url => !tabs.some(tab => tab.url === url));
-      if (urls.length === 0) {
-        window.close();
-      }
-
-      let tabsCreated = [];
-      urls.forEach(url => {
-        chrome.tabs.create({ url: url, active: false}, tab => {
-          tabsCreated.push(tab.id);
-          if (tabsCreated.length === urls.length) {
-            chrome.storage.local.set({ 'openingTabs': tabsCreated });
-            window.close();
-          }
-        });
-      });
-    });
-  });
-}
-
-document.getElementById('stash').addEventListener('click', stash);
-document.getElementById('apply').addEventListener('click', apply);
+document.getElementById('apply').addEventListener('click', () => {
+  chrome.runtime.sendMessage({'command': "apply_stash"});
+  window.close();
+});
